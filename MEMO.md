426b45a1c5dee02939c1669fcea0e553734c398e
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2021-05-01 18:44:50 +0900

    途中で進め方がわからなくなったので一旦諦める

---
+++ MEMO.md
@@ -0,0 +1,6 @@
+# メモ
+## 誤っている箇所
+P78
+DiskManager
+new, opemメソッドの変数名がdata_file になっているが、次ページではheap_fileになっている
+
---
+++ src/disk.rs
@@ -0,0 +1,105 @@
+use std::convert::TryInto;
+use std::fs::{File, OpenOptions};
+use std::io::{self, prelude::*, SeekFrom};
+use std::path::Path;
+
+use zerocopy::{AsBytes, FromBytes};
+
+pub const PAGE_SIZE: usize = 4096;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, FromBytes, AsBytes)]
+#[repr(C)]
+
+pub struct PageId(pub u64);
+impl PageId {
+    pub const INVALID_PAGE_ID: PageId = PageId(u64::MAX);
+
+    pub fn valid(self) -> Option<PageId> {
+        if self == Self::INVALID_PAGE_ID {
+            None
+        } else {
+            Some(self)
+        }
+    }
+
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
+}
+
+impl Default for PageId {
+    fn default() -> Self {
+        Self::INVALID_PAGE_ID
+    }
+}
+
+impl From<Option<PageId>> for PageId {
+    fn from(page_id: Option<PageId>) -> Self {
+        page_id.unwrap_or_default()
+    }
+}
+
+impl From<&[u8]> for PageId {
+    fn from(bytes: &[u8]) -> Self {
+        let arr = bytes.try_into().unwrap();
+        PageId(u64::from_ne_bytes(arr))
+    }
+}
+
+
+pub struct DiskManager {
+    // ヒープファイルのファイルディスクリプタ
+    heap_file: File,
+    // 採番するページIDを決めるカウンタ
+    next_page_id: u64,
+}
+
+impl DiskManager {
+    // コンストラクタ
+    pub fn new(heap_file: File) -> io::Result<Self> {
+        let heap_file_size = heap_file.metadata()?.len();
+        let next_page_id = heap_file_size / PAGE_SIZE as u64;
+        ok(Self {
+            heap_file,
+            next_page_id,
+        })
+    }
+    // ファイルパスを指定して開く
+    pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
+        let heap_file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(heap_file_path)?;
+        Self::new(heap_file)
+    }
+    // 新しいページIDを採番する
+    pub fn allocate_page(&mut self) -> PageId {
+        let page_id = self.next_page_id;
+        self.next_page_id += 1;
+        PageId(page_id)
+    }
+    // ページのデータを読み出す
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
+        // オフセットを計算
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
+        // ページ先頭へシーク
+        self.heap_file.seek(SeekFrom::Start(offset))?;
+        // データを読み出す
+        self.heap_file.read_exact(data)
+    }
+    // データをページに書き出す
+    pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
+        // オフセットを計算
+        let offset = PAGE_SIZE as u 64 * page_id.to_u64();
+        // ページ先頭へシーク
+        self.heap_file.seek(SeekFrom::Start(offset))?;
+        // データを書き込む
+        self.heap_file.write_all(data)
+    }
+
+    pub fn sync(&mut self) -> io::Result<()> {
+        self.heap_file.flush()?;
+        self.heap_file.sync_all()
+    }
+}
